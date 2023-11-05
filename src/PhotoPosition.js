de02535363dfe252d1ea2e686de551c0e0ac4bcf
Fahad <fahad@Fahads-MacBook-Air.local>
2023-11-06 03:18:21 +0600

    photo position updated

--- src/PhotoPosition.js
+++ src/PhotoPosition.js
@@ -6,6 +6,32 @@ const PhotoPosition = ({ photo, index, selectPhoto, reorderPhotos }) => {
     photo.selected ? 'selected' : ''
   } ${index === 0 ? 'first-photo' : ''}`; 
 
+  return (
+    <div
+      className={photoClass}
+      draggable
+      onDrag={(e) => e.preventDefault()}
+      onDragStart={(e) => e.dataTransfer.setData('text/plain', index)}
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        e.preventDefault();
+        const dragIndex = e.dataTransfer.getData('text/plain');
+        if (dragIndex !== index) {
+          reorderPhotos(parseInt(dragIndex), index);
+        }
+      }}
+    >
+      <img
+        src={`/images/${photo.src}`}
+        alt={`Photo ${photo.id}`}
+        onClick={() => selectPhoto(photo.id)}
+      />
+      <div className="overlay" onClick={() => selectPhoto(photo.id)}></div>
+    </div>
+  );
+
 };
 
 export default PhotoPosition;
